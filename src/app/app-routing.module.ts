ac62816a081d6ec5575361dcfe7e2d659def0ed1
Zurc <cruzjurado@gmail.com>
2022-02-09 11:29:05 +0000

    app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/playlist',
+    pathMatch: 'full',
+  },
+  {
     path: 'playlist',
     loadChildren: () =>
       import('./features/playlist/playlist.module').then(
         (m) => m.PlaylistModule
       ),
-  },
-  {
-    path: '',
-    redirectTo: '/playlist',
-    pathMatch: 'full',
   },
 ];
 
